0389f7f04f17420ca8762b9cc1aa318c44b07ab1
meghanabaddipudireddy <baddipudim829@gmail.com>
2026-07-30 16:48:07 -0700

    hazard detection and stalling added

--- src/top.sv
+++ src/top.sv
@@ -39,12 +39,19 @@ module top (
     logic        pc_src;
     logic [3:0]  alu_op;
 
+    // hazard signals
+    logic pc_write;
+    logic if_id_write;
+    logic bubble;
+
     //pc logic
     always_ff @(posedge clk) begin
-        if 
+        if begin
             (rst) pc <= 32'b0;
-        else
+        end
+        else if(pc_write) begin
             pc <= next_pc;
+        end
     end
 
     assign pc_4 = pc + 32'd4;
@@ -65,7 +72,7 @@ module top (
             if_id_pc <= 0;
             if_id_instr <= 0;
         end
-        else begin
+        else if (if_id_write) begin
             if_id_pc <= pc;
             if_id_instr <= instr;
         end
@@ -74,7 +81,7 @@ module top (
     //ID/EX register
     id_ex_t id_ex_reg;
     always_ff @(posedge clk) begin
-        if(rst) begin
+        if(rst || bubble) begin
             id_ex_reg <= '0;
         end
         else begin
@@ -136,6 +143,5 @@ module top (
     alu u_alu(.rd_1(id_ex_reg.rd_data_1), .rd_2(alu_input_b), .alu_op(id_ex_reg.alu_op), .zero(zero), .result(alu_res));
     data_memory u_data_memory( .clk(clk), .address(ex_mem_reg.alu_result), .wr_data(ex_mem_reg.rd_data_2), .mem_write(ex_mem_reg.mem_write), .mem_read(ex_mem_reg.mem_read), .read_data(mem_read_data));
     control_unit u_control_unit(.opcode(if_id_instr[6:0]), .funct3(if_id_instr[14:12]), .funct7(if_id_instr[31:25]), .zero(zero), .reg_write(reg_write), .alu_src(alu_src), .mem_write(mem_write), .mem_read(mem_read), .mem_to_reg(mem_to_reg), .pc_src(pc_src), .alu_op(alu_op));
-
-    //hello
+    hazard_unit u_hazard_unit(.rs1(if_id_instr[19:15]), .rs2(if_id_instr[24:20]), .id_ex_rd(id_ex_reg.rd), .id_ex_reg_write(id_ex_reg.reg_write), .ex_mem_rd(ex_mem_reg.rd), .ex_mem_reg_write(ex_mem_reg.reg_write), .pc_write(pc_write), .if_id_write(if_id_write), .bubble(bubble));
 endmodule
